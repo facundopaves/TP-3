27a09db51a280d08a2589508e2f603c9611ca8d4
facundopaves <179752458+facundopaves@users.noreply.github.com>
2024-09-18 21:02:59 -0300

    Add files via upload

---
+++ Flor.java
@@ -0,0 +1,34 @@
+public class Flor {
+
+    // Atributos privados de la clase
+    private String actitud;
+    private String apariencia;
+
+    // Constructor de la clase
+    public Flor(String actitud, String apariencia) {
+        this.actitud = actitud;
+        this.apariencia = apariencia;
+    }
+
+    // Método para obtener la actitud
+    public String getActitud() {
+        return actitud;
+    }
+
+    // Método para establecer la actitud
+    public void setActitud(String actitud) {
+        this.actitud = actitud;
+    }
+
+    // Método para obtener la apariencia
+    public String getApariencia() {
+        return apariencia;
+    }
+
+    // Método para establecer la apariencia
+    public void setApariencia(String apariencia) {
+        this.apariencia = apariencia;
+    }
+
+    
+}
