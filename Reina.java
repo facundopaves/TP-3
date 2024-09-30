24d7c8c4578f0cfac44e1a1ca3965ed7cbee6054
facundopaves <179752458+facundopaves@users.noreply.github.com>
2024-09-30 20:09:04 -0300

    Reina.java

--- Reina.java
+++ Reina.java
@@ -3,4 +3,11 @@ public class Reina extends Pieza {
     public Reina(String colorPieza, String movimiento, String caracter) {
         super(colorPieza, movimiento, caracter,"Lenta");
     }
+
+
+    @Override
+    
+    public String mover(){
+        return "Se mueve en diagonal como en horizontal y en vertical";
+    }
 }
