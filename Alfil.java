0b9a2bbac1281c9aa467678a003726453d01caef
facundopaves <179752458+facundopaves@users.noreply.github.com>
2024-09-30 20:07:17 -0300

    Alfil.java

--- Alfil.java
+++ Alfil.java
@@ -3,4 +3,10 @@ public class Alfil extends Pieza {
     public Alfil(String colorPieza, String movimiento, String caracter) {
         super(colorPieza, movimiento, caracter,"lenta");
     }
+
+    @Override //sobreescribo un metodo de la clase padre
+    
+    public String mover(){
+        return "se mueve en diagonal";
+    }
 }
