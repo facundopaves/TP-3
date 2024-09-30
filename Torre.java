11d6df65e84853d8a755fee4f30f53b61095d408
facundopaves <179752458+facundopaves@users.noreply.github.com>
2024-09-30 20:10:25 -0300

    Torre.java

--- Torre.java
+++ Torre.java
@@ -2,4 +2,11 @@ public class Torre extends Pieza {
     public Torre(String colorPieza, String movimiento, String caracter) {
         super(colorPieza, movimiento, caracter,"Lenta");
     }
+
+
+    @Override
+    
+    public String mover(){
+        return "Se mueve en linea recta, hacia delante y hacia atras";
+    }
 }
