4c90e863628c3b946089123b58c9093e97d89091
facundopaves <179752458+facundopaves@users.noreply.github.com>
2024-09-30 22:29:33 -0300

    Reina.java

--- Reina.java
+++ Reina.java
@@ -1,5 +1,8 @@
 //Defino una clase llamada Reina que extiende otra clase llamada pieza
 public class Reina extends Pieza {
+
+    public Reina(){} //Constructor vacio
+
     public Reina(String colorPieza, String movimiento, String caracter) {
         super(colorPieza, movimiento, caracter,"Lenta");
     }
