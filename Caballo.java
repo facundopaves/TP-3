b2c3288f294a9a9eb87d88d78908c13af1751590
facundopaves <179752458+facundopaves@users.noreply.github.com>
2024-09-30 22:28:33 -0300

    Caballo.java

--- Caballo.java
+++ Caballo.java
@@ -1,5 +1,8 @@
 //Defino una clase llamada caballo que extiende otra clase llamada pieza
 public class Caballo extends Pieza {
+
+    public Caballo(){} //Constructor vacio
+
     public Caballo(String colorPieza, String movimiento, String caracter) {
         super(colorPieza, movimiento, caracter, "Lenta"); //la clase super se utiliza para referirse a la clase padre,
         //en este caso el metodo super() llama al constructor de la clase Pieza
@@ -8,6 +11,6 @@ public class Caballo extends Pieza {
     @Override
     
     public String mover(){
-        return "Se mueve en L";
+        return "Me muevo en L";
     }
 }
