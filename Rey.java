622fbffc5c3fa28e61be30cd74bb1359e6ac35b2
facundopaves <179752458+facundopaves@users.noreply.github.com>
2024-09-30 22:29:47 -0300

    Rey.java

--- Rey.java
+++ Rey.java
@@ -1,5 +1,9 @@
 //Defino una clase llamada Rey que extiende otra clase llamada pieza
 public class Rey extends Pieza {
+
+    public Rey(){}//Constructor vacio
+
+
     public Rey(String colorPieza, String movimiento, String caracter) {
         super(colorPieza, movimiento, caracter,"Lenta");
     }
